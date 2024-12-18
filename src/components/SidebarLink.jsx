8a57c7f52fa0dcb0408bd0e1e5117359a377da4f
Luke_G_WSL <68129402+Smasheroonie@users.noreply.github.com>
2024-12-18 17:28:26 +0000

    start sorting

--- src/components/SidebarLink.jsx
+++ src/components/SidebarLink.jsx
@@ -4,7 +4,7 @@ export default function SidebarLink({ text }) {
   return (
     <Link
       to={{ pathname: `/topics/${text}` }}
-      className="flex max-[1710px]:pr-2 max-[1710px]:pb-1 my-0.5 items-center cursor-pointer rounded-lg border border-slate-400 hover:bg-sky-200 hover:text-cyan-950 hover:transition-colors active:bg-sky-100 ease-out duration-300"
+      className="bg-white flex max-[1710px]:pr-2 max-[1710px]:pb-1 my-0.5 items-center cursor-pointer rounded-lg border border-slate-400 hover:bg-sky-200 hover:text-cyan-950 hover:transition-colors active:bg-sky-100 ease-out duration-300"
     >
       <h2 className="font-bold text-xl my-4 ml-2">{text}</h2>
     </Link>
